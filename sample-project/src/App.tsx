922e0e334945f395c42243a50937ad4e683daf97
ESOLUTIONS\Sathish.Puttappa <sathish.puttappa@trivium-esolutions.com>
2023-05-25 13:37:27 +0530

    Add project files.

---
+++ sample-project/src/App.tsx
@@ -0,0 +1,9 @@
+import Message from './Component/Message';
+function App() {
+  return (
+    <Message />
+    
+     );
+  }
+
+export default App;
---
+++ sample-project/src/Component/Message.tsx
@@ -0,0 +1,7 @@
+function Message(){
+    const name = "Sathish"
+    if(name)
+    return <h1> Hi {name}</h1>
+    return <h1> Hi world</h1>
+}
+export default Message;
