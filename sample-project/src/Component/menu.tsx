901d554ebe663c57bddf55c2516c077750cbfad6
ESOLUTIONS\Sathish.Puttappa <sathish.puttappa@trivium-esolutions.com>
2023-05-26 13:31:53 +0530

    Used statemanagement to select the active item

--- sample-project/src/Component/menu.tsx
+++ sample-project/src/Component/menu.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 function Menu() {
   let listItem = ["Coffee", "Tea", "Milk", "Black Tea", "LemonTea"];
   //listItem = [];
+  const [active, setActive] = useState(-1);
 
   return (
     <>
@@ -9,9 +12,12 @@ function Menu() {
       <ul className="list-group">
         {listItem.map((x, index) => (
           <li
-            className="list-group-item"
+            className={
+              index === active ? "list-group-item active" : "list-group-item"
+            }
             key={x}
-            onClick={() => console.log(x + " " + index)}
+            //onClick={() => console.log(x + " " + index)}
+            onClick={() => setActive(index)}
           >
             {x}
           </li>
