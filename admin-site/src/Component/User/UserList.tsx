e4ff66dba51f2ef7cf8b4f1e85917b0ca19c0817
ESOLUTIONS\Sathish.Puttappa <sathish.puttappa@trivium-esolutions.com>
2023-06-01 12:55:16 +0530

    Added Backend api call business logic

--- admin-site/src/Component/User/UserList.tsx
+++ admin-site/src/Component/User/UserList.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { users } from "./User";
+import axios from "axios";
+//import { users } from "./User";
+interface Users {
+  id: number;
+  name: string;
+  username: string;
+  emilId: string;
+}
+
 const UsersList = () => {
+  const [users, setUsers] = useState<Users[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.messaage));
+  }, []);
+
   const navigate = useNavigate();
   const goDetails = () => {
     navigate("/user/e001");
@@ -8,23 +27,24 @@ const UsersList = () => {
 
   return (
     <>
+      {error && <p>{error}</p>}
       <tbody>
         <tr>
           <th>UID</th>
+          <th>Name</th>
           <th>UserName</th>
-          <th>CompanyId</th>
-          <th>CompanyName</th>
-          <th>UserPermission</th>
-          <th>Usertype</th>
+          <th>Email ID</th>
+          {/* <th>UserPermission</th>
+          <th>Usertype</th> */}
         </tr>
         {users.map((item, index) => (
           <tr key={index}>
-            <td>{item.uid}</td>
-            <td>{item.userName}</td>
-            <td>{item.companyId}</td>
-            <td>{item.companyName}</td>
-            <td>{item.userPermission}</td>
-            <td>{item.usertype}</td>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.username}</td>
+            <td>{item.emilId}</td>
+            {/* <td>{item.userPermission}</td>
+            <td>{item.usertype}</td> */}
             <td>
               <button onClick={goDetails}>Details</button>
             </td>
